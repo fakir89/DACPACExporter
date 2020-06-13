797efafec6fd703068033d2cfd817e050b6ad624
dmib <fakir89@mail.ru>
2020-06-13 18:12:43 +0300

    Add copiright.

--- DacPac Exporter/Properties/AssemblyInfo.cs
+++ DacPac Exporter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DACPAC Exporter")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 dmib")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
